bda509f50845331d46c6dbae9790c0571a89bdea
Sven Otto <sven.otto@gameduell.de>
2014-08-14 11:53:29 +0200

    Initial commit

---
+++ duell/setup/plugin/EnvironmentSetup.hx
@@ -0,0 +1,14 @@
+package duell.setup.plugin;
+
+class EnvironmentSetup
+{
+    public function new()
+    {
+
+    }
+
+    public function setup(args : Array<String>)
+    {
+        trace("EXTERNAL YEAHA");
+    }
+}
